fe1667bcbdca88dd9341ead922c667339bbc67fc
Tor Gammelgård <tor.gammelgard@gmail.com>
2016-10-26 16:10:49 +0200

    Fix sharing static resources, images, styles

--- src/main/java/se/torgammelgard/web/config/WebConfig.java
+++ src/main/java/se/torgammelgard/web/config/WebConfig.java
@@ -1,42 +1,27 @@
 package se.torgammelgard.web.config;
 
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
-import org.springframework.web.context.ServletConfigAware;
 import org.springframework.web.context.ServletContextAware;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 
 @Configuration
 @ComponentScan("se.torgammelgard.web")
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware, ServletContextAware {
-
-    private ApplicationContext applicationContext;
+public class WebConfig extends WebMvcConfigurerAdapter implements ServletContextAware {
 
     private ServletContext servletContext;
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
 
     @Override
     public void setServletContext(ServletContext servletContext) {
@@ -72,4 +57,13 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
         return templateResolver;
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/resources/images/");
+        registry.addResourceHandler("/styles/")
+                .addResourceLocations("/resources/styles/");
+    }
 }
