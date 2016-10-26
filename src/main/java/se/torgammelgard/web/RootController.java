053368d950db64f77dcf6236c92a2b0be53d7c7d
Tor Gammelgård <tor.gammelgard@gmail.com>
2016-10-26 15:41:07 +0200

    Add to Thymeleaf in index

--- src/main/java/se/torgammelgard/web/RootController.java
+++ src/main/java/se/torgammelgard/web/RootController.java
@@ -1,14 +1,21 @@
 package se.torgammelgard.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/")
 public class RootController {
 
+    private DateFormat dateFormat = DateFormat.getDateInstance();
+
     @RequestMapping
-    public String root() {
+    public String root(Model model) {
+        model.addAttribute("serverTime", dateFormat.format(new Date()));
         return "index";
     }
 }
