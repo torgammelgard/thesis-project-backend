532c442c2028c849de89ab3f29b4b1b362f9c2f8
Tor Gammelgård <tor.gammelgard@gmail.com>
2016-10-30 16:28:20 +0100

    Trying to add sets to match

--- src/main/java/se/torgammelgard/web/MatchPathController.java
+++ src/main/java/se/torgammelgard/web/MatchPathController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import se.torgammelgard.persistence.entities.Match;
+import se.torgammelgard.persistence.entities.TennisSet;
+import se.torgammelgard.persistence.entities.TennisSetScore;
 import se.torgammelgard.service.MatchService;
 import se.torgammelgard.service.TeamService;
+import se.torgammelgard.service.TennisSetScoreService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/match")
@@ -17,6 +23,9 @@ public class MatchPathController {
 
     @Autowired
     private TeamService teamService;
+
+    @Autowired
+    private TennisSetScoreService tennisSetScoreService;
 
     @Autowired
     private MatchService matchService;
@@ -29,17 +38,31 @@ public class MatchPathController {
 
     @RequestMapping("/add_form_page")
     public String addMatch(Model model) {
+        model.addAttribute("setscore", new TennisSetScore());
         model.addAttribute("match", new Match());
         model.addAttribute("teams", teamService.findAll());
         return "add_match";
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String matchmng(@ModelAttribute Match match, BindingResult bindingResult, Model model) {
+    public String matchmng(@ModelAttribute Match match, @ModelAttribute TennisSetScore score, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("errors", bindingResult.getAllErrors());
             return "error_page";
         }
+
+        score = tennisSetScoreService.save(score);
+        List<TennisSetScore> tennisSetScores = new ArrayList<>(0);
+        tennisSetScores.add(score);
+
+        TennisSet tennisSet = new TennisSet();
+        tennisSet.setTennisSetScore(tennisSetScores);
+
+        List<TennisSet> tennisSets = new ArrayList<>(0);
+        tennisSets.add(tennisSet);
+
+        match.setTennisSets(tennisSets);
+
         matchService.save(match);
         return "redirect:/api/match";
     }
